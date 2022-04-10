5f5ccddfff0c834fda87d7d99d99994dc9b3ed85
RakAsrevnel <jeanbaptistemaupin@gmail.com>
2022-04-10 11:24:08 +0200

    mouche Tetris + ajout d'un bouton de spawn pour test + modif mouche etrange

--- Assets/Scripts/Spawnpoint.cs
+++ Assets/Scripts/Spawnpoint.cs
@@ -78,4 +78,66 @@ public class Spawnpoint : MonoBehaviour
     {
         isSpawning = false;
     }
+
+    [Button]
+    public void Spawn0()
+    {
+        Instantiate(enemiesToSpawn[0], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn1()
+    {
+        Instantiate(enemiesToSpawn[1], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn2()
+    {
+        Instantiate(enemiesToSpawn[2], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn3()
+    {
+        Instantiate(enemiesToSpawn[3], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn4()
+    {
+        Instantiate(enemiesToSpawn[4], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn5()
+    {
+        Instantiate(enemiesToSpawn[5], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn6()
+    {
+        Instantiate(enemiesToSpawn[6], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn7()
+    {
+        Instantiate(enemiesToSpawn[7], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn8()
+    {
+        Instantiate(enemiesToSpawn[8], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn9()
+    {
+        Instantiate(enemiesToSpawn[9], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn10()
+    {
+        Instantiate(enemiesToSpawn[10], transform.position, Quaternion.identity);
+    }
+    [Button]
+    public void Spawn11()
+    {
+        Instantiate(enemiesToSpawn[11], transform.position, Quaternion.identity);
+    }
+
 }
